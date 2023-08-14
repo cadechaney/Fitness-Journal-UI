8ea4ee12ac36044d46fa6546af98aa2b655ebb5d
Cade Chaney <cadechaney@live.com>
2023-08-14 05:19:48 -0600

    Add successful post

--- src/App.js
+++ src/App.js
@@ -8,27 +8,49 @@ function App() {
       response => response.json()
     ).then(
       data => {
-        console.log(data)
+        console.log(data.workouts)
       }
     )
   }, [])
 
   const postRequest = () => {
-    fetch('/stuff', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({stuff: 'post'})
-    })
-    .then(response => response.json())
-    .then(result => {
-      console.log('Response', result)
-    })
-    .catch(error => {
-      console.log('Error', error)
-    })
-  }
+    // Fetch the current data first to have the current state of "stuff" array
+    fetch("/stuff")
+      .then(response => response.json())
+      .then(data => {
+        // Create a new object to add to the "stuff" array
+        const newObject = { key: 'value' }; // Replace with your new object data
+  
+        // Add the new object to the "stuff" array
+        const updatedStuff = [...data.workouts.stuff, newObject];
+  
+        // Send the updated data to the server
+        fetch('/stuff', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ workouts: { stuff: updatedStuff } })
+        })
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              console.log('Broken');
+            }
+          })
+          .then(result => {
+            console.log('Response', result);
+          })
+          .catch(error => {
+            console.log('Error', error);
+          });
+      })
+      .catch(error => {
+        console.log('Error fetching current data', error);
+      });
+  };
+  
 
   return (
     <div className="App">
