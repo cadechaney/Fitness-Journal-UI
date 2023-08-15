d3be88364d454ff477f9248d3d74744907222af9
Cade Chaney <cadechaney@live.com>
2023-08-15 10:11:03 -0600

    Add nav link to scroll workout component for every workout

--- src/components/scrollWorkouts/scrollWorkouts.js
+++ src/components/scrollWorkouts/scrollWorkouts.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './scrollWorkouts.css'
+import { NavLink } from 'react-router-dom'
 
 function scrollWorkout({ workouts, deleteWorkout }) {
   
-
   return (
     <div className='scrollWorkout-container'>
       {workouts.length > 0 ? (
         <section>
           {workouts.slice(0, 7).map((workout, index) => (
-            <div key={index}>
-              <section className='workout-info-small'>
-                <p>{workout.title}</p>
-                <p>{workout.date}</p>
-                <button className='delete-workout' onClick={() => deleteWorkout(index)}>Delete</button>
-              </section>
-            </div>
+            <NavLink key={index} to={`/workout/${index}`}>
+              <div key={index}>
+                <section className='workout-info-small'>
+                  <p>{workout.title}</p>
+                  <p>{workout.date}</p>
+                  <button className='delete-workout' onClick={() => deleteWorkout(index)}>Delete</button>
+                </section>
+              </div>
+            </NavLink>
           ))}
         </section>
       ) : (
