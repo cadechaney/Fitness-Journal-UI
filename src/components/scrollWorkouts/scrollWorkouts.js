1cb9e01a124eeb578bc536a6d730594dd6358d55
Cade Chaney <cadechaney@live.com>
2023-08-15 11:51:49 -0600

    Add styling to workout scroll view

--- src/components/scrollWorkouts/scrollWorkouts.js
+++ src/components/scrollWorkouts/scrollWorkouts.js
@@ -9,15 +9,15 @@ function scrollWorkout({ workouts, deleteWorkout, filterWorkout }) {
       {workouts.length > 0 ? (
         <section>
           {workouts.slice(0, 7).map((workout, index) => (
-            <NavLink key={index} to={`/workout/${index}`}>
-              <div key={index} onClick={() => filterWorkout(index)}>
-                <section className='workout-info-small'>
-                  <p>{workout.title}</p>
-                  <p>{workout.date}</p>
-                  <button className='delete-workout' onClick={() => deleteWorkout(index)}>Delete</button>
-                </section>
+            <div key={index} onClick={() => filterWorkout(index)} className='single-scroll-workout'>
+                <NavLink key={index} to={`/workout/${index}`} className='workout-link'>
+                  <section className='workout-info-small'>
+                    <p>{workout.title}</p>
+                    <p className='date-tag'>{workout.date}</p>
+                  </section>
+                </NavLink>
+                <button className='delete-workout' onClick={() => deleteWorkout(index)}>Delete</button>
               </div>
-            </NavLink>
           ))}
         </section>
       ) : (
