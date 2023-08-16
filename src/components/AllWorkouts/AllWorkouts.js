db3c51bfa7b648ee2801ff082a987598d6bd84dc
Cade Chaney <cadechaney@live.com>
2023-08-16 15:51:23 -0600

    Add styling to all workouts list

--- src/components/AllWorkouts/AllWorkouts.js
+++ src/components/AllWorkouts/AllWorkouts.js
@@ -5,23 +5,28 @@ import { NavLink } from 'react-router-dom'
 function AllWorkouts({ allWorkouts, filterWorkout, deleteWorkout }) {
 
   return (
-    <div className='all-workouts-container'>
-      {allWorkouts.map((workout, index) => (
-        <div key={index} onClick={() => filterWorkout(index)} className='all-single-scroll-workout'>
-            <NavLink key={index} to={`/workout/${index}`} className='all-workout-link'>
-              <div className='all-title-tag-container'>
-                <p className='all-title-tag'>{workout.title}</p>
+    <main className='allWorkouts-header-container'>
+      <header className='past-workouts-header'>
+        <h1 className='past-workouts-title'>All Past Workouts</h1>
+      </header>
+      <div className='all-workouts-container'>
+        {allWorkouts.map((workout, index) => (
+          <div key={index} onClick={() => filterWorkout(index)} className='all-single-scroll-workout'>
+              <NavLink key={index} to={`/workout/${index}`} className='all-workout-link'>
+                <div className='all-title-tag-container'>
+                  <p className='all-title-tag'>{workout.title}</p>
+                </div>
+                <div className='all-date-tag-container'>
+                  <p className='all-date-tag'>{workout.date}</p>
+                </div>
+              </NavLink>
+              <div className='all-delete-button-container'>
+                <button className='all-delete-workout' onClick={() => deleteWorkout(index)}>Delete</button>
               </div>
-              <div className='all-date-tag-container'>
-                <p className='all-date-tag'>{workout.date}</p>
-              </div>
-            </NavLink>
-            <div className='all-delete-button-container'>
-              <button className='all-delete-workout' onClick={() => deleteWorkout(index)}>Delete</button>
             </div>
-          </div>
-      )).reverse()}
-    </div>
+        )).reverse()}
+      </div>
+    </main>
   )
 }
 
