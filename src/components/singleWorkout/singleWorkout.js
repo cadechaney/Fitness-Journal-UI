4b38eafddda416708242fdab200fa3c3c28b97d2
Cade Chaney <cadechaney@live.com>
2023-08-16 09:09:40 -0600

    Add arrow icon to assets folder

--- src/components/singleWorkout/singleWorkout.js
+++ src/components/singleWorkout/singleWorkout.js
@@ -3,9 +3,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function SingleWorkout({ singleWorkout }) {
-
   const navigate = useNavigate()
-
   const workout = singleWorkout[0]
 
   return (
