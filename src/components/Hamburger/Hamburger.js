b91005b44e69bf92ce84d9d5f60d6e691d47eaa5
Cade Chaney <cadechaney@live.com>
2023-08-17 21:30:51 -0600

    Add settings image and link to component

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Hamburger.css';
 import { NavLink } from 'react-router-dom';
 import Hamburger from '../../assets/hamburger-icon-512x365-t1in8msh.png'
+import settingsIcon from '../../assets/126472.png'
 
 function HamburgerMenu() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -19,6 +20,9 @@ function HamburgerMenu() {
         <div className="dropdown-content">
           <NavLink className='drop-down-option' to='/past/workouts'>
             <p>View All Past Workouts</p>
+          </NavLink>
+          <NavLink className='drop-down-option' to='/settings'>
+            <img className='settings-image' src={settingsIcon} alt='settings-icon'></img>
           </NavLink>
         </div>
       )}
