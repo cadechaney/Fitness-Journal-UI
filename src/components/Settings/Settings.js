fde08971aa3bbd7a2267d8f06c650f9380bf611d
Cade Chaney <cadechaney@live.com>
2023-08-17 21:40:54 -0600

    Add skeleton to the settings component

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -4,7 +4,14 @@ import React from 'react'
 function Settings() {
 
   return (
-    <p>Settings</p>
+    <div className='settings-container'>
+      <nav className='settings-nav-bar'>
+        <h4 className='delete-account-title'>Delete Account</h4>
+      </nav>
+      <main>
+        <p>This will contain the form to delete account</p>
+      </main>
+    </div>
   )
 }
 
