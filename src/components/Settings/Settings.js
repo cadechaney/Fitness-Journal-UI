c12fae732253db6f267a380731da58c0f3f74aa5
Cade Chaney <cadechaney@live.com>
2023-08-18 08:19:16 -0600

    Add base skeleton styling to account deletion page

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -29,23 +29,35 @@ function Settings({ loggedInUsername, userPassword, loggedInUserId }) {
       <nav className='settings-nav-bar'>
         <h4 className='delete-account-title-nav'>Delete Account</h4>
       </nav>
-      <main>
-        <h1 className='delete-accouunt-title'>Delete Account?</h1>
-        <label>Account Username:</label>
-        <input 
-          type='text' 
-          placeholder='username'
-          value={inputUsername}
-          onChange={(e) => setInputUsername(e.target.value)}
-          ></input>
-        <label>Password:</label>
-        <input 
-          type='password' 
-          placeholder='password'
-          value={inputPassword}
-          onChange={(e) => setInputPassword(e.target.value)}
-        ></input>
-        <button className='delete-account-button' onClick={handleAccountDelete}>Delete</button>
+      <main className='delete-account-main'>
+        <header className='account-deletion-header'>
+          <h1 className='delete-account-title'>Delete Account?</h1>
+        </header>
+        <div className='account-deletion-form-container'>
+          <section className='account-deletion-form'>
+            <p>Please fill out the form below to delete your account</p>
+            <label>Account Username:</label>
+            <input 
+              className='account-deletion-username-input'
+              type='text' 
+              placeholder='username'
+              value={inputUsername}
+              onChange={(e) => setInputUsername(e.target.value)}
+              ></input>
+            <label>Password:</label>
+            <input 
+              className='account-deletion-password-input'
+              type='password' 
+              placeholder='password'
+              value={inputPassword}
+              onChange={(e) => setInputPassword(e.target.value)}
+            ></input>
+            <section className='account-delete-button-container'>
+              <div></div>
+              <button className='delete-account-button' onClick={handleAccountDelete}>Delete</button>
+            </section>
+          </section>
+        </div>
       </main>
     </div>
   )
